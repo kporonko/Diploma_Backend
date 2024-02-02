a33f9a3588355dc26efa048e818a18543ae3b273
Kyryl Poronko <poronko2045@gmail.com>
2024-02-02 20:28:34 +0200

    XML documentation

--- Diploma.Backend.API/Diploma.Backend.Application/Mappers/LoginMapper.cs
+++ Diploma.Backend.API/Diploma.Backend.Application/Mappers/LoginMapper.cs
@@ -8,8 +8,16 @@ using System.Threading.Tasks;
 
 namespace Diploma.Backend.Application.Mappers
 {
+    /// <summary>
+    /// Mapper for login workflow.
+    /// </summary>
     public static class LoginMapper
     {
+        /// <summary>
+        /// Creates the LoginResponse object from access token.
+        /// </summary>
+        /// <param name="token">Access token.</param>
+        /// <returns>Login response object filled with access token.</returns>
         public static LoginResponse CreateTokenResponse(string token)
         {
             return new LoginResponse
