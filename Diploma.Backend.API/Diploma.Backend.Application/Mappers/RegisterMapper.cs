f758f8a545f74031e347fb96fb8e1e932476de74
Kyryl Poronko <poronko2045@gmail.com>
2024-02-02 20:28:34 +0200

    XML documentation

--- Diploma.Backend.API/Diploma.Backend.Application/Mappers/RegisterMapper.cs
+++ Diploma.Backend.API/Diploma.Backend.Application/Mappers/RegisterMapper.cs
@@ -11,8 +11,16 @@ using System.Threading.Tasks;
 
 namespace Diploma.Backend.Application.Mappers
 {
+    /// <summary>
+    /// Mapper for register workflow.
+    /// </summary>
     public static class RegisterMapper
     {
+        /// <summary>
+        /// Creates the LoginResponse object from access token.
+        /// </summary>
+        /// <param name="token">Access token.</param>
+        /// <returns>Login response object filled with access token.</returns>
         public static LoginResponse CreateTokenResponse(string token)
         {
             return new LoginResponse
@@ -24,6 +32,11 @@ namespace Diploma.Backend.Application.Mappers
             };
         }
 
+        /// <summary>
+        /// Converts the register request to user entity.
+        /// </summary>
+        /// <param name="registerRequest">User register data.</param>
+        /// <returns>User entity filled with register data.</returns>
         public static User ConvertRegisterToUser(RegisterRequest registerRequest)
         {
             return new User
