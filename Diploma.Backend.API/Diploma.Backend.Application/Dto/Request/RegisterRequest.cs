feb990184a36260fac9734b36f9030fb8ec01d46
Kyryl Poronko <poronko2045@gmail.com>
2024-02-02 21:03:06 +0200

    Validation added

--- Diploma.Backend.API/Diploma.Backend.Application/Dto/Request/RegisterRequest.cs
+++ Diploma.Backend.API/Diploma.Backend.Application/Dto/Request/RegisterRequest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,9 +9,20 @@ namespace Diploma.Backend.Application.Dto.Request
 {
     public class RegisterRequest
     {
+        [Required(ErrorMessage = "You must enter your email address.")]
+        [EmailAddress]
         public string Email { get; set; }
+
+        [StringLength(50, MinimumLength = 8, ErrorMessage = "Password must contain at least 8 symbols.")]
+        [DataType(DataType.Password)]
         public string Password { get; set; }
+
+        [Required(ErrorMessage = "You must enter your first name.")]
+        [StringLength(50, MinimumLength = 2, ErrorMessage = "First name must contain at least 2 symbols.")]
         public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "You must enter your last name.")]
+        [StringLength(50, MinimumLength = 2, ErrorMessage = "Last name must contain at least 2 symbols.")]
         public string LastName { get; set; }
     }
 }
