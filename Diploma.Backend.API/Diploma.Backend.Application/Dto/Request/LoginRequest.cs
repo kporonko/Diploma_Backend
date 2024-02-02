54675ed5b2fc2b3ae8fae0f7ed8230c18002c7a6
Kyryl Poronko <poronko2045@gmail.com>
2024-02-02 21:03:06 +0200

    Validation added

--- Diploma.Backend.API/Diploma.Backend.Application/Dto/Request/LoginRequest.cs
+++ Diploma.Backend.API/Diploma.Backend.Application/Dto/Request/LoginRequest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,12 @@ namespace Diploma.Backend.Application.Dto.Request
 {
     public class LoginRequest
     {
-        
+        [Required(ErrorMessage = "You must enter your email address.")]
+        [EmailAddress]
         public string Email { get; set; }
+
+        [StringLength(50, MinimumLength = 8, ErrorMessage = "Password must contain at least 8 symbols.")]
+        [DataType(DataType.Password)]
         public string Password { get; set; }
     }
 }
