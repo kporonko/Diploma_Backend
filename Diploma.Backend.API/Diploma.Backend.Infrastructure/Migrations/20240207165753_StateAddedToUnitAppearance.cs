1bd4dd835684840eb8bfaa64d6f1904e1ee1bb86
Kyryl Poronko <poronko2045@gmail.com>
2024-02-07 19:01:04 +0200

    State field to unit appearance

---
+++ Diploma.Backend.API/Diploma.Backend.Infrastructure/Migrations/20240207165753_StateAddedToUnitAppearance.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Diploma.Backend.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class StateAddedToUnitAppearance : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "State",
+                table: "UnitAppearance",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "State",
+                table: "UnitAppearance");
+        }
+    }
+}
