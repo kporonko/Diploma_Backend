74bed3348a73dc95479cbb2e778a9bb143c42caf
Kyryl Poronko <poronko2045@gmail.com>
2024-02-10 14:06:49 +0200

    Targeting crud, get countries

---
+++ Diploma.Backend.API/Diploma.Backend.Application/Dto/Response/TargetingCreateResponse.cs
@@ -0,0 +1,17 @@
+﻿using Diploma.Backend.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Diploma.Backend.Application.Dto.Response
+{
+    public class TargetingCreateResponse
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<int> CountriesIds { get; set; }
+        public List<int>? SurveysIds { get; set; }
+    }
+}
