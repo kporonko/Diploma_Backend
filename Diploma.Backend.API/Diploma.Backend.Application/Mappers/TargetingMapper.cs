c9eafcb10583f555fe47fb3dcd9e8e472e088fb4
Kyryl Poronko <poronko2045@gmail.com>
2024-02-10 14:06:49 +0200

    Targeting crud, get countries

---
+++ Diploma.Backend.API/Diploma.Backend.Application/Mappers/TargetingMapper.cs
@@ -0,0 +1,35 @@
+﻿using Diploma.Backend.Application.Dto.Request;
+using Diploma.Backend.Application.Dto.Response;
+using Diploma.Backend.Domain.Models;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Diploma.Backend.Application.Mappers
+{
+    public static class TargetingMapper
+    { 
+        public static Targeting ConvertTargetingCreateRequestToTargeting(TargetingCreateRequest targetingCreateRequest, int userId)
+        {
+            return new Targeting
+            {
+                Name = targetingCreateRequest.Name,
+                UserId = userId           
+            };
+        }
+
+        public static TargetingCreateResponse MapTargetingToResponse(Targeting targeting)
+        {
+            return new TargetingCreateResponse
+            {
+                Id = targeting.Id,
+                Name = targeting.Name,
+                CountriesIds = targeting.CountryInTargetings.Select(c => c.CountryId).ToList(),
+                SurveysIds = !targeting.Surveys.IsNullOrEmpty() ? targeting.Surveys.Select(s => s.Id).ToList() : null
+            };
+        }
+    }
+}
