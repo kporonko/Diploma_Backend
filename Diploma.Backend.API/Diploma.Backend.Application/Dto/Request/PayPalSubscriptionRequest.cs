d2f6b4735d7fcc97ba8312dd1ff01ea53f245d48
Kyryl Poronko <poronko2045@gmail.com>
2024-05-27 00:57:46 +0300

    Initial paypal subscription flows impl, subscription added to db

---
+++ Diploma.Backend.API/Diploma.Backend.Application/Dto/Request/PayPalSubscriptionRequest.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Diploma.Backend.Application.Dto.Request
+{
+    public class PayPalSubscriptionRequest
+    {
+        public string plan_id { get; set; }
+        public DateTime start_time { get; set; }
+        //public string quantity { get; set; }
+        public PayPalShippingAmount shipping_amount { get; set; }
+        public PayPalSubscriber subscriber { get; set; }
+        public PayPalApplicationContext application_context { get; set; }
+    }
+
+    public class PayPalShippingAmount
+    {
+        public string currency_code { get; set; }
+        public string value { get; set; }
+    }
+
+    public class PayPalSubscriber
+    {
+        public PayPalName name { get; set; }
+        public string email_address { get; set; }
+        public PayPalShippingAddress shipping_address { get; set; }
+    }
+
+    public class PayPalName
+    {
+        public string given_name { get; set; }
+        public string surname { get; set; }
+    }
+
+    public class PayPalNameFull
+    {
+        public string full_name { get; set; }
+    }
+
+    
+    public class PayPalShippingAddress
+    {
+        public PayPalNameFull name { get; set; }
+        public PayPalAddress address { get; set; }
+    }
+
+    public class PayPalAddress
+    {
+        public string address_line_1 { get; set; }
+        public string address_line_2 { get; set; }
+        public string admin_area_2 { get; set; }
+        public string admin_area_1 { get; set; }
+        public string postal_code { get; set; }
+        public string country_code { get; set; }
+    }
+
+    public class PayPalApplicationContext
+    {
+        public string brand_name { get; set; }
+        public string locale { get; set; }
+        public string shipping_preference { get; set; }
+        public string user_action { get; set; }
+        public PayPalPaymentMethod payment_method { get; set; }
+        public string return_url { get; set; }
+        public string cancel_url { get; set; }
+    }
+
+    public class PayPalPaymentMethod
+    {
+        public string payer_selected { get; set; }
+        public string payee_preferred { get; set; }
+    }
+}
---
+++ Diploma.Backend.API/Diploma.Backend.Infrastructure/Migrations/20240526215643_Subscription.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Diploma.Backend.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Subscription : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Subscription",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    SubscriptionId = table.Column<string>(type: "varchar(255)", nullable: true),
+                    Name = table.Column<string>(type: "varchar(255)", nullable: false),
+                    DateCreate = table.Column<DateTime>(type: "datetime", nullable: false),
+                    DateChangeStatus = table.Column<DateTime>(type: "datetime", nullable: false),
+                    CurrencyCode = table.Column<string>(type: "varchar(255)", nullable: false),
+                    IsActive = table.Column<bool>(type: "bit", nullable: false),
+                    Price = table.Column<decimal>(type: "money", nullable: false),
+                    UserId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Subscription", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Subscription_User_UserId",
+                        column: x => x.UserId,
+                        principalTable: "User",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Subscription_UserId",
+                table: "Subscription",
+                column: "UserId",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Subscription");
+        }
+    }
+}
