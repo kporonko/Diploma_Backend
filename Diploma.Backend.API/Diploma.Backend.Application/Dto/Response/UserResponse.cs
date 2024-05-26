3694d12edef33595827e7a3650833c12b7c50de2
Kyryl Poronko <poronko2045@gmail.com>
2024-05-27 01:09:45 +0300

    Fixed user request: added user subscription info

---
+++ Diploma.Backend.API/Diploma.Backend.Application/Dto/Response/UserResponse.cs
@@ -0,0 +1,19 @@
+﻿using Diploma.Backend.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Diploma.Backend.Application.Dto.Response
+{
+    public class UserResponse
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Role { get; set; }
+        public Subscription Subscription { get; set; }
+    }
+}
---
+++ Diploma.Backend.API/Diploma.Backend.Infrastructure/Services/impl/UserService.cs
@@ -0,0 +1,39 @@
+﻿using Diploma.Backend.Application.Dto.Response;
+using Diploma.Backend.Application.Helpers;
+using Diploma.Backend.Application.Mappers;
+using Diploma.Backend.Application.Services;
+using Diploma.Backend.Domain.Common;
+using Diploma.Backend.Domain.Models;
+using Diploma.Backend.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Diploma.Backend.Infrastructure.Services.impl
+{
+    public class UserService : IUserService
+    {
+        private readonly ApplicationContext _context;
+        private IConfiguration _config;
+
+        public UserService(ApplicationContext context,
+            IConfiguration config)
+        {
+            _context = context;
+            _config = config;
+        }
+
+        public async Task<BaseResponse<UserResponse>> GetUserData(User userJwt)
+        {
+            var subscription = await _context.Subscriptions
+                .Include(s => s.User)
+                .FirstOrDefaultAsync(x => x.UserId == userJwt.Id);
+
+            return BaseResponseGenerator.GenerateValidBaseResponse(UserMapper.CreateUserDataResponse(subscription));
+        }
+    }
+}
